a3a76f011e86a0dbfd54c7329ac51e13a498ee7f
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-02-04 23:23:58 -0300

    Create ex017.js

---
+++ aula13/ex017.js
@@ -0,0 +1,21 @@
+var c = 1
+do{
+    console.log("Olá, Mundo! " + c)
+    c++
+} while (c < 6)
+
+
+/*var c = 1
+while (c < 6){
+    console.log("Olá, Mundo! " + c)
+    c++
+}*/
+
+
+
+/*console.log("Olá, Mundo!")
+console.log("Olá, Mundo!")
+console.log("Olá, Mundo!")
+console.log("Olá, Mundo!")
+console.log("Olá, Mundo!")
+console.log("Olá, Mundo!")*/
